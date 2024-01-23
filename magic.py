81ea28ac6ef04e22d95284f97394bf8ddc5780a8
Strunin Andrey <strunin10@rambler.ru>
2024-01-23 21:58:34 +0300

    qwerty

---
+++ magic.py
@@ -0,0 +1,32 @@
+import telebot
+import random
+
+#6954817418:AAHyr5LrxPkNIHQAqsf_WaWoGLr93yn4QCM
+#Magicball187_bot
+#создать список случайных фраз-ответов, которые будет давать "магический шар"
+
+answers = ["Конечно.......да", "Конечно.......нет🤥", "Ну не знаю...", "Ответ на этот вопрос знает только Вселенная😝", "Несомненно", "Секрет🤫🤫🤫", "Сомнительно, но okey", "Я не являюсь политиком и никогда им не был. Меня политика не интересует", "Я сказал, и много что сказал. Не надо мне пожаласта это предъявлять"]
+
+bot = telebot.TeleBot("6954817418:AAHyr5LrxPkNIHQAqsf_WaWoGLr93yn4QCM")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот")
+
+@bot.message_handler(commands=["hello", "hi"])
+def greeting(message):
+    bot.reply_to(message, "Приветик")
+
+@bot.message_handler(commands=["stop", "bye"])
+def parting(message):
+    bot.reply_to(message, "Пока-пока")
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    user_text = message.text
+    if "?" in user_text:
+        bot.reply_to(message, f"{random.choice(answers)}")
+    else:
+        bot.reply_to(message, f"Задай любой вопрос, я отвечу")
+
+bot.polling(none_stop=True, interval=0)
